eedfc243acbdc632e6568f67da57f4d2a9539fa6
manujagobind <manujagobind@gmail.com>
2018-02-18 16:24:22 +0530

    GET and POST using APIView.

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from profiles_api import models
+from . import models
 
 
 admin.site.register(models.UserProfile)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import serializers
+
+
+class HelloAPIView(APIView):
+    """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
+        an_apiview = [
+            'Uses HTTP methods as function',
+            'Similar to traditional Django view',
+            'Gives most control over your logic',
+            'Mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'api_view': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello, {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
